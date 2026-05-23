edb9048458081d9e26221ce2a4b0b7d495b24d75
leookot09-ai <leo.okot09@gmail.com>
2026-05-23 06:20:42 +0000

    Merge remote changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/global.css' // <--- Import global styles here
+import App from './MkCityRockGym.jsx' // 👈 Make sure this matches your filename
+import './styles/global.css' // If you have this file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
